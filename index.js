fc7b487611e66789a9ac8cf005055d47c7e74978
Nata <nataliiazaporozhets@gmail.com>
2020-05-10 12:33:16 +0300

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+var express = require("express"); // отдельная переменная для удобства
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/public', express.static('public')); 
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('send message', function(msg) {
+	io.emit('receive message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
